29f273e9980670da64d9d72d86ddedd7736c42bb
gollowars <yoshizumi.ashikawa@gmail.com>
2022-02-03 01:31:08 +0900

    study BLoc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+// import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'dart:async';
+// import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,18 +15,97 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Text(ref.read(stringStateProvider))
     return MaterialApp(
       title: "Welcome to Flutter",
       home: Scaffold(
         appBar: AppBar(title: const Text("hello")),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text("STATELESS TEXT"), 
-            MyContainer(title: "ashikawa",)
-          ],
-        )),
+        body: Provider<CounterBloc>(
+            create: (context) => CounterBloc(),
+            dispose: (context, value) => value.dispose(),
+            child: const MyBody()),
+      ),
+    );
+  }
+}
+
+class CounterBloc {
+  // final _actionController = StreamController<void>();
+  // Sink<void> get increment => _actionController.sink;
+
+  // final _decrementController = StreamController<void>();
+  // Sink<void> get decrement => _decrementController.sink;
+
+  final _countController = StreamController<int>();
+  Stream<int> get count => _countController.stream;
+
+  int _count = 0;
+
+  void increment() {
+    _count++;
+    _countController.sink.add(_count);
+  }
+
+  void decrement() {
+    _count--;
+    _countController.sink.add(_count);
+  }
+
+  void clear() {
+    _count = 0;
+    _countController.sink.add(_count);
+  }
+
+  CounterBloc() {
+    // _actionController.stream.listen((_) {
+    //   _count++;
+    //   _countController.sink.add(_count);
+    // });
+  }
+
+  void dispose() {
+    // _actionController.close();
+    _countController.close();
+  }
+}
+
+class MyBody extends StatelessWidget {
+  const MyBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final counterBloc = Provider.of<CounterBloc>(context);
+    void add() {
+      counterBloc.increment();
+    }
+
+    void remove() {
+      counterBloc.decrement();
+    }
+
+    void clear() {
+      counterBloc.clear();
+    }
+
+    return Center(
+      child: Column(
+        children: [
+          const Text('Study BLoC'),
+          StreamBuilder(
+              initialData: 0,
+              stream: counterBloc.count,
+              builder: (context, snapshot) {
+                return Text("Count is : ${snapshot.data}");
+              }),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: add, child: const Text('Inc')),
+                ElevatedButton(onPressed: remove, child: const Text('Dec')),
+                ElevatedButton(onPressed: clear, child: const Text('Clear'))
+              ],
+            )
+        ],
       ),
     );
   }
@@ -43,11 +128,9 @@ class _MyContainerState extends State<MyContainer> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children:  [
-          Text("count : $_counter "),
-          ElevatedButton(onPressed: _incrementCounter, child: const Icon(Icons.add))
-        ]);
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Text("count : $_counter "),
+      ElevatedButton(onPressed: _incrementCounter, child: const Icon(Icons.add))
+    ]);
   }
 }
