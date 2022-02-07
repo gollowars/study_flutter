87cfdd62af22fc7f311171dcf221954a5d1358be
J-WIN\gollowars <yoshizumi.ashikawa@gmail.com>
2022-02-07 18:57:05 +0900

    update

--- lib/bloc/user_bloc.dart
+++ lib/bloc/user_bloc.dart
@@ -1,21 +1,28 @@
+import 'dart:ffi';
+
 import 'package:netroll/models/user.dart';
 import 'package:netroll/repositories/api_repository.dart';
 import 'package:rxdart/rxdart.dart';
 
 class UsersBloc {
   final _subject = PublishSubject<UserResponse>();
+  final _loadingSubject = BehaviorSubject<bool>.seeded(false);
 
   UsersBloc();
 
   Stream<UserResponse> get stream => _subject.stream;
   Sink<UserResponse> get sink => _subject.sink;
+  Stream<bool> get isLoadingStream => _loadingSubject.stream;
 
   void fetch() async {
+    _loadingSubject.add(true);
     final _response = await UsersRepository().fetch();
     sink.add(_response);
+    _loadingSubject.add(false);
   }
 
   void dispose() {
     _subject.close();
+    _loadingSubject.close();
   }
 }
